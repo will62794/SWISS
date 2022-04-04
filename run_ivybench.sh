d0b471f8927e50407efe9feb53f74928054a71c1
William Schultz <will627@comcast.net>
2022-04-03 21:58:17 -0400

    Add some more protocols

--- run_ivybench.sh
+++ run_ivybench.sh
@@ -1,4 +1,35 @@
-#!/bin/sh
+
+# bms = [
+#     tla-consensus.pyv
+#     tla-tcommit.pyv
+#     ex-quorum-leader
+#     toy_consensus_forall.pyv ,
+#     tla-simple.pyv
+#     ex-lockserv-automaton.pyv
+#     tla-simpleregular.pyv
+#     sharded_kv.pyv ,
+#     lockserv.pyv ,
+#     tla-twophase.pyv
+#     learning_switch.pyv ,
+#     simple-decentralized-lock
+#     two_phase_commit.pyv ,
+#     consensus_wo_decide.pyv ,
+#     consensus_forall.pyv ,
+#     pyv-learning-switch
+#     chord.pyv ,
+#     sharded_kv_no_lost_keys.pyv ,
+#     naive_consensus.ivy ,
+#     client_server_ae.pyv ,
+#     simple-election.pyv
+#     toy_consensus_epr.pyv ,
+#     toy_consensus.pyv ,
+#     client_server_db_ae.pyv ,
+#     hybrid_reliable_broadcast.pyv ,
+#     firewall.pyv ,
+#     majorityset_leader_election.pyv ,
+#     consensus_epr.pyv ,
+# ]
+
 ./run.sh benchmarks/tla-consensus.pyv --config auto --threads 1 --minimal-models --with-conjs
 ./run.sh benchmarks/tla-tcommit.pyv --config auto --threads 1 --minimal-models --with-conjs
 ./run.sh benchmarks/ex-quorum-leader-election.pyv --config auto --threads 1 --minimal-models --with-conjs
