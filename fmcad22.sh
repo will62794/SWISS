13a4874f77023cd5302202b3102b90a1f745891a
William Schultz <will627@comcast.net>
2022-08-11 22:54:42 -0400

    Mark benchmarks that are not run

--- fmcad22.sh
+++ fmcad22.sh
@@ -7,65 +7,65 @@
 bmdir="benchmarks/fmcad22"
 
 # Run with 'auto' config.
-./run.sh $bmdir/ex-lockserv_automaton.pyv --config auto --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/ex-majorityset-leader-election.pyv --config auto --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/ex-naive_consensus.pyv --config auto --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/ex-quorum-leader-election.pyv --config auto --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/ex-simple-decentralized-lock.pyv --config auto --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/ex-simple-election.pyv --config auto --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/ex-toy_consensus.pyv --config auto --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/i4-chord_ring_maintenance.pyv --config auto --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/i4-learning_switch.pyv --config auto --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/i4-lock_server.pyv --config auto --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/i4-two_phase_commit.pyv --config auto --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/pyv-client_server_ae.pyv --config auto --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/pyv-client_server_db_ae.pyv --config auto --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/pyv-consensus_epr.pyv --config auto --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/pyv-consensus_forall.pyv --config auto --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/pyv-consensus_wo_decide.pyv --config auto --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/pyv-firewall.pyv --config auto --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/pyv-hybrid_reliable_broadcast.pyv --config auto --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/pyv-learning_switch.pyv --config auto --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/pyv-lockserv.pyv --config auto --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/pyv-sharded_kv.pyv --config auto --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/pyv-sharded_kv_no_lost_keys.pyv --config auto --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/pyv-toy_consensus_epr.pyv --config auto --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/pyv-toy_consensus_forall.pyv --config auto --threads 1 --minimal-models --with-conjs
 ./run.sh $bmdir/tla-Consensus.pyv --config auto --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/tla-Simple.pyv --config auto --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/tla-SimpleRegular.pyv --config auto --threads 1 --minimal-models --with-conjs
 ./run.sh $bmdir/tla-TCommit.pyv --config auto --threads 1 --minimal-models --with-conjs
+# ./run.sh $bmdir/i4-lock_server.pyv --config auto --threads 1 --minimal-models --with-conjs # reported in SWISS NSDI21 paper.
+./run.sh $bmdir/ex-quorum-leader-election.pyv --config auto --threads 1 --minimal-models --with-conjs
+# ./run.sh $bmdir/pyv-toy_consensus_forall.pyv --config auto --threads 1 --minimal-models --with-conjs # reported in SWISS NSDI21 paper.
+./run.sh $bmdir/tla-Simple.pyv --config auto --threads 1 --minimal-models --with-conjs
+./run.sh $bmdir/ex-lockserv_automaton.pyv --config auto --threads 1 --minimal-models --with-conjs
+./run.sh $bmdir/tla-SimpleRegular.pyv --config auto --threads 1 --minimal-models --with-conjs
+# ./run.sh $bmdir/pyv-sharded_kv.pyv --config auto --threads 1 --minimal-models --with-conjs # reported in SWISS NSDI21 paper.
+# ./run.sh $bmdir/pyv-lockserv.pyv --config auto --threads 1 --minimal-models --with-conjs # reported in SWISS NSDI21 paper.
 ./run.sh $bmdir/tla-TwoPhase.pyv --config auto --threads 1 --minimal-models --with-conjs
+./run.sh $bmdir/i4-learning_switch.pyv --config auto --threads 1 --minimal-models --with-conjs
+./run.sh $bmdir/ex-simple-decentralized-lock.pyv --config auto --threads 1 --minimal-models --with-conjs
+# ./run.sh $bmdir/i4-two_phase_commit.pyv --config auto --threads 1 --minimal-models --with-conjs # reported in SWISS NSDI21 paper.
+# ./run.sh $bmdir/pyv-consensus_wo_decide.pyv --config auto --threads 1 --minimal-models --with-conjs # reported in SWISS NSDI21 paper.
+# ./run.sh $bmdir/pyv-consensus_forall.pyv --config auto --threads 1 --minimal-models --with-conjs # reported in SWISS NSDI21 paper.
+# ./run.sh $bmdir/pyv-learning_switch.pyv --config auto --threads 1 --minimal-models --with-conjs # reported in SWISS NSDI21 paper.
+# ./run.sh $bmdir/i4-chord_ring_maintenance.pyv --config auto --threads 1 --minimal-models --with-conjs # reported in SWISS NSDI21 paper.
+# ./run.sh $bmdir/pyv-sharded_kv_no_lost_keys.pyv --config auto --threads 1 --minimal-models --with-conjs # reported in SWISS NSDI21 paper.
+./run.sh $bmdir/ex-naive_consensus.pyv --config auto --threads 1 --minimal-models --with-conjs
+# ./run.sh $bmdir/pyv-client_server_ae.pyv --config auto --threads 1 --minimal-models --with-conjs # reported in SWISS NSDI21 paper.
+./run.sh $bmdir/ex-simple-election.pyv --config auto --threads 1 --minimal-models --with-conjs
+# ./run.sh $bmdir/pyv-toy_consensus_epr.pyv --config auto --threads 1 --minimal-models --with-conjs # reported in SWISS NSDI21 paper.
+./run.sh $bmdir/ex-toy_consensus.pyv --config auto --threads 1 --minimal-models --with-conjs
+# ./run.sh $bmdir/pyv-client_server_db_ae.pyv --config auto --threads 1 --minimal-models --with-conjs # reported in SWISS NSDI21 paper.
+# ./run.sh $bmdir/pyv-hybrid_reliable_broadcast.pyv --config auto --threads 1 --minimal-models --with-conjs # reported in SWISS NSDI21 paper.
+./run.sh $bmdir/pyv-firewall.pyv --config auto --threads 1 --minimal-models --with-conjs
+./run.sh $bmdir/ex-majorityset-leader-election.pyv --config auto --threads 1 --minimal-models --with-conjs
+./run.sh $bmdir/pyv-consensus_epr.pyv --config auto --threads 1 --minimal-models --with-conjs
 ./run.sh $bmdir/mldr_epr_15.ivy --config auto --threads 1 --minimal-models --with-conjs
 
 # Run with 'templ' config.
-./run.sh $bmdir/ex-lockserv_automaton.pyv --config templ --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/ex-majorityset-leader-election.pyv --config templ --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/ex-naive_consensus.pyv --config templ --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/ex-quorum-leader-election.pyv --config templ --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/ex-simple-decentralized-lock.pyv --config templ --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/ex-simple-election.pyv --config templ --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/ex-toy_consensus.pyv --config templ --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/i4-chord_ring_maintenance.pyv --config templ --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/i4-learning_switch.pyv --config templ --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/i4-lock_server.pyv --config templ --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/i4-two_phase_commit.pyv --config templ --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/pyv-client_server_ae.pyv --config templ --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/pyv-client_server_db_ae.pyv --config templ --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/pyv-consensus_epr.pyv --config templ --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/pyv-consensus_forall.pyv --config templ --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/pyv-consensus_wo_decide.pyv --config templ --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/pyv-firewall.pyv --config templ --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/pyv-hybrid_reliable_broadcast.pyv --config templ --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/pyv-learning_switch.pyv --config templ --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/pyv-lockserv.pyv --config templ --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/pyv-sharded_kv.pyv --config templ --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/pyv-sharded_kv_no_lost_keys.pyv --config templ --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/pyv-toy_consensus_epr.pyv --config templ --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/pyv-toy_consensus_forall.pyv --config templ --threads 1 --minimal-models --with-conjs
 ./run.sh $bmdir/tla-Consensus.pyv --config templ --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/tla-Simple.pyv --config templ --threads 1 --minimal-models --with-conjs
-./run.sh $bmdir/tla-SimpleRegular.pyv --config templ --threads 1 --minimal-models --with-conjs
 ./run.sh $bmdir/tla-TCommit.pyv --config templ --threads 1 --minimal-models --with-conjs
+# ./run.sh $bmdir/i4-lock_server.pyv --config templ --threads 1 --minimal-models --with-conjs # reported in SWISS NSDI21 paper.
+./run.sh $bmdir/ex-quorum-leader-election.pyv --config templ --threads 1 --minimal-models --with-conjs
+# ./run.sh $bmdir/pyv-toy_consensus_forall.pyv --config templ --threads 1 --minimal-models --with-conjs # reported in SWISS NSDI21 paper.
+./run.sh $bmdir/tla-Simple.pyv --config templ --threads 1 --minimal-models --with-conjs
+./run.sh $bmdir/ex-lockserv_templmaton.pyv --config templ --threads 1 --minimal-models --with-conjs
+./run.sh $bmdir/tla-SimpleRegular.pyv --config templ --threads 1 --minimal-models --with-conjs
+# ./run.sh $bmdir/pyv-sharded_kv.pyv --config templ --threads 1 --minimal-models --with-conjs # reported in SWISS NSDI21 paper.
+# ./run.sh $bmdir/pyv-lockserv.pyv --config templ --threads 1 --minimal-models --with-conjs # reported in SWISS NSDI21 paper.
 ./run.sh $bmdir/tla-TwoPhase.pyv --config templ --threads 1 --minimal-models --with-conjs
+./run.sh $bmdir/i4-learning_switch.pyv --config templ --threads 1 --minimal-models --with-conjs
+./run.sh $bmdir/ex-simple-decentralized-lock.pyv --config templ --threads 1 --minimal-models --with-conjs
+# ./run.sh $bmdir/i4-two_phase_commit.pyv --config templ --threads 1 --minimal-models --with-conjs # reported in SWISS NSDI21 paper.
+# ./run.sh $bmdir/pyv-consensus_wo_decide.pyv --config templ --threads 1 --minimal-models --with-conjs # reported in SWISS NSDI21 paper.
+# ./run.sh $bmdir/pyv-consensus_forall.pyv --config templ --threads 1 --minimal-models --with-conjs # reported in SWISS NSDI21 paper.
+# ./run.sh $bmdir/pyv-learning_switch.pyv --config templ --threads 1 --minimal-models --with-conjs # reported in SWISS NSDI21 paper.
+# ./run.sh $bmdir/i4-chord_ring_maintenance.pyv --config templ --threads 1 --minimal-models --with-conjs # reported in SWISS NSDI21 paper.
+# ./run.sh $bmdir/pyv-sharded_kv_no_lost_keys.pyv --config templ --threads 1 --minimal-models --with-conjs # reported in SWISS NSDI21 paper.
+./run.sh $bmdir/ex-naive_consensus.pyv --config templ --threads 1 --minimal-models --with-conjs
+# ./run.sh $bmdir/pyv-client_server_ae.pyv --config templ --threads 1 --minimal-models --with-conjs # reported in SWISS NSDI21 paper.
+./run.sh $bmdir/ex-simple-election.pyv --config templ --threads 1 --minimal-models --with-conjs
+# ./run.sh $bmdir/pyv-toy_consensus_epr.pyv --config templ --threads 1 --minimal-models --with-conjs # reported in SWISS NSDI21 paper.
+./run.sh $bmdir/ex-toy_consensus.pyv --config templ --threads 1 --minimal-models --with-conjs
+# ./run.sh $bmdir/pyv-client_server_db_ae.pyv --config templ --threads 1 --minimal-models --with-conjs # reported in SWISS NSDI21 paper.
+# ./run.sh $bmdir/pyv-hybrid_reliable_broadcast.pyv --config templ --threads 1 --minimal-models --with-conjs # reported in SWISS NSDI21 paper.
+./run.sh $bmdir/pyv-firewall.pyv --config templ --threads 1 --minimal-models --with-conjs
+./run.sh $bmdir/ex-majorityset-leader-election.pyv --config templ --threads 1 --minimal-models --with-conjs
+./run.sh $bmdir/pyv-consensus_epr.pyv --config templ --threads 1 --minimal-models --with-conjs
 ./run.sh $bmdir/mldr_epr_15.ivy --config templ --threads 1 --minimal-models --with-conjs
